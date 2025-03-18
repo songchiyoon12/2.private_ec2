9d130eea8b37ff40cc49047c20c1a9f7e8f6fc6b
songchiyoon12 <thdcldbs1234@gmail.com>
2025-03-18 22:03:21 +0900

    commit

---
+++ instance.tf
@@ -0,0 +1,59 @@
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners = ["amazon"]
+
+  filter {
+    name = "name"
+    values = ["al2023-ami-2023.*-x86_64"]
+  }
+
+  filter {
+    name = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+
+}
+
+
+resource "aws_instance" "private_ec2" {
+  ami           = data.aws_ami.amazon_linux_2.id
+  instance_type = "t3.small"
+
+  subnet_id = module.vpc.private_subnets[0]
+
+  security_groups = [aws_security_group.allow_tls.id]
+
+
+  tags = {
+    Name = "ec2"
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = module.vpc.vpc_id
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
---
+++ variables.tf
@@ -0,0 +1,3 @@
+variable "region" {
+  default = "ap-northeast-2"                     # The type of the variable, in this case a string
+}
